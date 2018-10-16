2b2a28ec7c58cf3928e1a61debb60fff3b40f40c
SAJJID ATTA G <sajjidattag@gmail.com>
2018-10-17 00:51:06 +0500

    Created FeedNotifier Project with Partial Implementation

---
+++ FeedNotifier/DataStructure/FeedItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Web;
+using System.IO;
+using System.Net;
+
+namespace FeedNotifier
+{
+    [Serializable, XmlRoot("item")]
+    public class FeedItem : IComparable
+    {
+        //#TODO:Sajjid Atta: Updae Class Fields and Methods
+        [XmlElement("title")]
+        public string Title { get; set; }
+        [XmlElement("link")]
+        public string Link { get; set; }
+        [XmlElement("description")]
+        public string Description { get; set; }
+        [XmlElement("pubDate")]
+        public string PublishedDate { get; set; }
+
+        //I have created this function to Remove html from Description, Ignore non recent items
+        internal FeedItem FurnishFeedItem()
+        {
+            FeedItem temp = new FeedItem();
+            temp.Description = WebUtility.HtmlDecode(Description); //To Decode &amp; etc
+            temp.Description = Regex.Replace(Description, "<.+?>", string.Empty);
+            
+            temp.Title = Title;
+            temp.PublishedDate = PublishedDate;
+            temp.Link = Link;
+            return temp;
+
+        }
+        public void Print()
+        {
+            System.Console.WriteLine("Title: " + Title);
+            System.Console.WriteLine("Publication Date: " + PublishedDate);
+            System.Console.WriteLine("Description: " + Description);
+            System.Console.WriteLine();
+        }
+      
+
+        public int CompareTo(object obj)
+        {
+            FeedItem fitem = (FeedItem)obj;
+            string parseFormat = "ddd, dd MMM yyyy HH:mm:ss zzz";
+            DateTime fstPubDate = DateTime.ParseExact(Convert.ToString(PublishedDate), parseFormat, CultureInfo.InvariantCulture);
+            DateTime scndPubDate = DateTime.ParseExact(Convert.ToString(fitem.PublishedDate), parseFormat, CultureInfo.InvariantCulture);
+            return scndPubDate.CompareTo(fstPubDate);
+        }
+
+     
+    }
+}
---
+++ FeedNotifier/Notifier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedNotifier
+{
+    public class Notifier
+    {
+        SortedSet<FeedItem> feedItems;
+        public Notifier()
+        {
+            feedItems = new SortedSet<FeedItem>();
+        }
+        public void PickRecentFeedItems(FeedItem [] feeditems)
+        {
+            foreach(FeedItem fitem in feeditems)
+            {
+                feedItems.Add(fitem);
+            }
+        }
+        public void Notify()
+        {
+            //TODO: To Improve | A lot of work in one method
+
+            StringBuilder MessageSubject=new StringBuilder(""); ;
+            StringBuilder MessageBody=new StringBuilder("");
+            int itr = 1;
+
+            //Create Message Subject and Body for new Jobs
+            foreach(FeedItem fitem in feedItems)
+            {
+                if (IsRecent(fitem))
+                {
+                    MessageSubject = new StringBuilder( String.Format("Upwork Notifier - {0} Jobs", itr++));
+                    MessageBody.Append(String.Format("Job Link: {0} {3} Time: {1} {3} Description: {2} {3}", fitem.Link, fitem.PublishedDate, fitem.Description, Environment.NewLine));
+                }
+                
+            }
+
+            //Send Email
+            SendEmail(MessageSubject.ToString(), MessageBody.ToString());
+        }
+
+        private void SendEmail(String messageSubject, String messageBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsRecent(FeedItem fitem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
